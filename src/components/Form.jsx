016c677f5fd571d7c1bab70708ec76dcd01347c6
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2021-02-26 22:56:04 -0300

    conectando a la base de datos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,83 +1,167 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Register from './Register';
 import './css-components/Form.css';
 
 
 const Form = () => {
-    
+
     const [register, setRegister] = useState(true);
-    
+
     const aRegistro = () => setRegister(false);
 
+    const volverAComponentePrincipal = e => setRegister(true);
 
-    return ( 
+    const [ingreso, setIngreso] = useState({
+
+        email: '',
+        password: ''
+
+    });
+
+    const capturarIngreso = (e) => {
+        setIngreso ({
+            ...ingreso,
+            [e.target.name]: [e.target.value]
+        
+        })
+        
+    }
+
+    const { email, password } = ingreso;
+
+
+    const verificarIngreso = async e => {
+
+        e.preventDefault();
+
+        if (email === '' || password === "") alert('ingrese un email valido.');
+
+
+        await traerDatos(email, password);
+
+    }
+
+
+
+
+
+
+
+    const traerDatos = async (email,password) => {
+
+        const key = '2e13ca84ec3be8b443b97f8c41fa734afda647f8de0276537ef58ad1b9ab7a66';
+        const url = `https://api-users.000webhostapp.com/api-usuarios/get.php?key=${key}`;
+
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: `email=${email}&password=${password}`
+        };
+
+        await fetch(url, options)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (err) {
+                console.error(err);
+            });
+    }
+
+
+
+   
+
+
+
+
+    return (
         <>
-        {register ?   
-        
-        <main className = "ingreso">
-            <form className = "ingreso__formulario">
-           
-                <div className = "ingreso__contenedor">
+            {register ?
 
-                    <p className = "ingreso__text">Usuario o email:</p>
+                <main className="ingreso">
+                    <form 
+                        
+                        className="ingreso__formulario"
+                        onSubmit ={verificarIngreso}
+                        
+                        
+                        
+                        >
 
-                        <input 
-                        type = "text" 
-                        className = "ingreso__usuario" 
-                    />
+                        <div className="ingreso__contenedor">
 
-                    <p className = "ingreso__text">Password:</p>
+                            <p className="ingreso__text">Usuario o email:</p>
 
-                    <input 
+                            <input
+                                type="text"
+                                className="ingreso__usuario"
+                                name="email"
+                                onChange = {capturarIngreso}
+                            
+                            />
 
-                        type ="password" 
-                        className = "ingreso__usuario" 
+                            <p className="ingreso__text">Password:</p>
 
-                    />
+                            <input
 
-                    <button 
-                        type = "submit" 
-                        className = "ingreso__button"
-
-                    >Ingresar</button>
-
-
-
-                    <button
-
-
-                        type = "submit" 
-                        className = "ingreso__button--nuevoUsuario"
-                        onClick = {aRegistro}
+                                type="password"
+                                className="ingreso__usuario"
+                                name="password"
+                                onChange = {capturarIngreso}
 
 
-                    >Crear cuenta nueva</button>  
-            
-            </div>
+                            />
 
-        
+                            <button
+                                type="submit"
+                                className="ingreso__button"
 
-            </form>
-        
-        </main>
-        
-        
-        
-        :<Register/> }
-        
-        
-        
-        
-        
-        
-        
-        
+                            >Ingresar</button>
+
+
+
+                            <button
+
+
+                                type="submit"
+                                className="ingreso__button--nuevoUsuario"
+                                onClick={aRegistro}
+
+
+                            >Crear cuenta nueva</button>
+
+                        </div>
+
+
+
+                    </form>
+
+                </main>
+
+
+
+                : <Register
+
+                    volverAComponentePrincipal={volverAComponentePrincipal}
+
+                />}
+
+
+
+
+
+
+
+
         </>
 
 
 
 
-     );
+    );
 }
- 
+
 export default Form;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,116 +1,144 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './css-components/Register.css';
 
 
 
-const Register = () => {
-    
-    const [almacenarDatos, setAlmacenarDatos] = useState([]);
+const Register = ({ volverAComponentePrincipal }) => {
 
-    const [datos,setDatos ] = useState({
+    const [datos, setDatos] = useState({
 
-        usuario: '',
+        user: '',
         email: '',
         password: ''
-        
+
     });
- 
-    
-    
+
     const capturarInput = e => {
 
         setDatos({
+
             ...datos,
-            [e.target.name]:[e.target.value]        
+            [e.target.name]: [e.target.value]
+
         })
-    
+
+
     }
 
-    const {usuario,email,password}  = datos;
+    const { user, email, password } = datos;
 
+    const guardarUsuario = async (user, email, password) => {
+        const key = '2e13ca84ec3be8b443b97f8c41fa734afda647f8de0276537ef58ad1b9ab7a66';
+        const url = `https://api-users.000webhostapp.com/api-usuarios/add.php?key=${key}`;
 
-    const RegistrarUsuario = e => {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: `user=${user}&email=${email}&password=${password}`
+        };
+
+        await fetch(url, options)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (err) {
+                console.error(err);
+            });
+    }
+
+    const registrarUsuario = async e => {
 
         e.preventDefault();
 
-        if(usuario === "" || email === '' || password === "") alert('ingrese un email valido.');
+        if (user === "" || email === '' || password === "") alert('ingrese un email valido.');
 
-        setAlmacenarDatos([datos])
+
+        await guardarUsuario(user, email, password);
+
+
+        volverAComponentePrincipal();
 
     }
-    
-    return ( 
+
+
+    return (
         <>
-       <main className = "registro">
-            <form 
-                className = "registro__formulario"
-                onSubmit = {RegistrarUsuario}
-            
-            
-            
-            >
-           
-                <div className = "registro__contenedor">
-
-                    <p className = "registro__text">Usuario:</p>
-
-                        <input 
-                        type = "text" 
-                        className = "registro__input" 
-                        name = 'usuario'
-                        onChange = {capturarInput}
+            <main className="registro">
+                <form
+                    className="registro__formulario"
+                    onSubmit={registrarUsuario}
 
 
 
-                    />
+                >
 
-                    <p className = "registro__text">Email:</p>
+                    <div className="registro__contenedor">
 
-                    <input 
+                        <p className="registro__text">Usuario:</p>
 
-                        type ="text" 
-                        className = "registro__input"
-                        name = 'email'
-                        onChange = {capturarInput}
-                         
+                        <input
+                            type="text"
+                            className="registro__input"
+                            name='user'
+                            onChange={capturarInput}
 
-                    />
 
-                    <p className = "registro__text">Password:</p>
-                        
-                    <input 
-                    
-                        type ="password" 
-                        className = "registro__input" 
-                        name = 'password'
-                        onChange = {capturarInput}
-                        
-                    />
 
-                    <button 
-                        type = "submit" 
-                        className = "registro__button"
+                        />
 
-                    >Registrarse</button>
-                    
+                        <p className="registro__text">Email:</p>
+
+                        <input
+
+                            type="text"
+                            className="registro__input"
+                            name='email'
+                            onChange={capturarInput}
+
+
+                        />
+
+                        <p className="registro__text">Password:</p>
+
+                        <input
+
+                            type="password"
+                            className="registro__input"
+                            name='password'
+                            onChange={capturarInput}
+
+                        />
+
+                        <button
+
+                            type="submit"
+                            className="registro__button"
+
+
+
+                        >Registrarse</button>
+
 
 
                     </div>
 
-        
 
-            </form>
-        
-        </main>
 
-        
-        
-  
+                </form>
+
+            </main>
+
+
+
+
         </>
 
 
     );
 
 }
- 
+
 export default Register;
